88e7dc980b9c64857b2f2440871c271bfa187357
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-11-21 19:22:37 +0500

    feat: create server action to create an invoice

---
+++ app/lib/action.ts
@@ -0,0 +1,39 @@
+'use server';
+
+// Zod is a TypeScript-first validation library
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+//  defining a schema that matches the shape of form object
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData:FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+  });
+    // converting amount into cents
+    const amountInCents = amount * 100;
+    // creating a new date with the format "YYYY-MM-DD" for the invoice's creation date
+    const date = new Date().toISOString().split('T')[0];
+    // Inserting data in database
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+    
+    revalidatePath('/dashboard/invoices');
+      redirect('/dashboard/invoices');
+
+}
